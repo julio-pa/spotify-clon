9769c365236ddf3963abbdc2e043c2033b01de62
Julio Patiño <franjp132@gmail.com>
2022-11-22 10:01:05 -0500

    Conectando la app con redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 import Login from './components/Login/login';
 import Player from './components/Player/Player';
-import { getTokenFromURL } from './spotifyLogic'
+import { selectToken, SET_TOKEN } from './Features/TokenSlice';
+import { selectUser, SET_USER } from './Features/UserSlice';
+import { getTokenFromURL } from './spotifyLogic';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
 
-  const [token, setToken] = useState("");
+  const dispatch = useDispatch();
+
+  const token = useSelector(selectToken)
+  const user = useSelector(selectUser)
 
   useEffect(() => {
     const hash = getTokenFromURL();
-    // window.location.hash = "";
     const _token = hash.access_token;
-    setToken(_token);
-    console.log("token =>", token)
-  }, [token]);
+    if (_token) {
+      dispatch(SET_TOKEN(_token))
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => dispatch(SET_USER(user)));
+      console.log(token);
+      spotify.getPlaylist().then(playlist => console.log(playlist))
+    }
+
+  }, [dispatch, token]);
 
   return (
     <div>
       {
-        token ? <Player/> : <Login/>
+        user ? <Player /> : <Login />
       }
     </div>
   );
